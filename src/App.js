360cc71707b9984d1e90137e702b8cb846e17b0a
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2020-03-16 16:00:08 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { withAuthenticator } from 'aws-amplify-react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World, this is V2!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(App, { includeGreetings: true })
