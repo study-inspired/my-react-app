dd37c5dfe93fe3deb1f3477de2154032c3a4968c
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2020-03-16 16:54:18 -0700

    removed amplify backend

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World, this is V2!</h1>
+        <h1>Hello World, this is V3!</h1>
       </header>
     </div>
   );
